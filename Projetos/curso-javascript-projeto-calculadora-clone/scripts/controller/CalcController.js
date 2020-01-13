da411f630e121486830d9d231e51687916ea7ec6
Lucas Beserra da Silva <lucasbeserrasilva18@gmail.com>
2020-01-13 11:20:54 -0300

    changes

--- Projetos/curso-javascript-projeto-calculadora-clone/scripts/controller/CalcController.js
+++ Projetos/curso-javascript-projeto-calculadora-clone/scripts/controller/CalcController.js
@@ -1,3 +1,49 @@
 class CalcController {
+    constructor(){
+        
+        this._displayCalc = document.querySelector("#display");
+        this._dataEl = document.querySelector("#data");
+        this._timeEl = document.querySelector("#hora");
+        this._currentDate;
+        this.initialize();
+
+    }
+
+    initialize(){
+        
+    }
+
+    get displayTime(){
+        return this._timeEl.innerHTML;
+    }
     
+    set displayTime(value){
+        this._timeEl.innerHTML = value;
+    }
+
+    get displayDate(){
+        return this._dataEl.innerHTML;
+    }
+
+    set displayDate(value){
+        this._dataEl.innerHTML = value;
+    }
+
+    get displayCalc(){
+        return this._displayCalc.innerHTML;
+    }
+
+    set displayCalc(value){
+        this._displayCalc.innerHTML = value;
+    }
+
+    get currentDate(){
+        return new Date();
+    }
+
+    set currentDate(value){
+        this._currentDate = value;
+    }
+
+
 }
